77a5f03746c6ba4a09364d5dd924644f83a6d99d
Adithya C <adithyachakka@gmail.com>
2024-07-27 18:04:04 -0400

    stuff

--- src/api/tastyApi.js
+++ src/api/tastyApi.js
@@ -1,28 +1,5 @@
 import axios from 'axios';
 
-const options = {
-  method: 'GET',
-  url: 'https://tasty.p.rapidapi.com/recipes/list',
-  params: {
-    from: '0',
-    size: '20',
-    tags: 'under_30_minutes'
-  },
-  headers: {
-    'x-rapidapi-key': '3f9a8179e3msh9af5dfd2d4a1521p1a275bjsn26410302489e',
-    'x-rapidapi-host': 'tasty.p.rapidapi.com'
-  }
-};
-
-try {
-	const response = await axios.request(options);
-	console.log(response.data);
-} catch (error) {
-	console.error(error);
-}
-
-import axios from 'axios';
-
 const tastyApi = axios.create({
   baseURL: 'https://tasty.p.rapidapi.com',
   headers: {
